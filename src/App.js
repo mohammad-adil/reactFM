369049daf41304e4c848efa1214b8603b5029f6a
mohammad-adil <shahaadil13@gmail.com>
2021-04-22 11:56:08 +0530

    Sharing Data between Components

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+const Pet = (props)=>{
+    return React.createElement("div",{},
+    [React.createElement('h1',{},props.name),
+    React.createElement('h1',{},props.animal),
+    React.createElement('h1',{},props.breed)]);
+};
+
+const  App =()=> {
+
+    return React.createElement("div", {id:"main-elm"},
+    [React.createElement("h1",{},"Adopt Me!!"),
+        React.createElement(Pet,{
+            name:"Luna",
+            animal:"Dog",
+            breed:"havanease"
+
+        }),
+        React.createElement(Pet,{
+            name:"Bella",
+            animal:"Cat",
+            Breed:"Persian"
+        }),
+        React.createElement(Pet,{
+            name:"birdy",
+            animal:"Bird",
+            breed:"Mixed"
+        })
+    ]);
+};
+ReactDOM.render(React.createElement(App),
+document.getElementById("root"))
